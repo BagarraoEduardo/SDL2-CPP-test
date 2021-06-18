165cdaf193d2ee516f5a2980cff5eb12bffdf7e6
Eduardo Bagarrão <general@eduardobagarrao.com>
2021-06-18 19:34:17 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# fabamaq-application
+
+Created and implemented by Eduardo Filipe Figueiredo Bagarrão, not only the code but all the assets.
+
+Using CMake for portability to other platforms.
+
---
+++ fabamaq-application-sfml/fabamaq-application-sfml.cpp
@@ -0,0 +1,12 @@
+﻿// fabamaq-application-sfml.cpp : Defines the entry point for the application.
+//
+
+#include "fabamaq-application-sfml.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
